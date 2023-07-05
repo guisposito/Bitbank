650c16870bcf6c0287b43d07c73b9d9a48746ca5
guisposito <guicarlosjoinhas@gmail.com>
2023-07-05 19:28:06 -0300

    Return e early return

--- index.js
+++ index.js
@@ -6,24 +6,27 @@ class Cliente {
 
 class ContaCorrente {
     agencia;
-    #saldo = 0;
+    _saldo = 0;
     
     sacar(valorSaque){
-        if(this.#saldo < valorSaque){
-            console.log("Voce não pode sacar o dinheiro")
+        if(this._saldo < valorSaque){
+            console.log("Voce não pode sacar o dinheiro");
         }else {
-            this.#saldo -= valorSaque;
-            console.log("Seu novo saldo é = " + this.#saldo)
+            this._saldo -= valorSaque;
+            console.log("Saque de " + valorSaque + " realizado com sucesso!");
+            console.log("Seu novo saldo é = " + this._saldo);
+            return valorSaque;
         }
     }
 
     depositar(valorDeposito){
         if(valorDeposito < 0){
-            console.log("Você não pode depositar este valor")
+            console.log("Você não pode depositar este valor");
         }else {
-            this.#saldo += valorDeposito;
+            this._saldo += valorDeposito;
             console.log("Deposito de " + valorDeposito + " depositado com sucesso!");
-            console.log("Seu novo saldo é = " + this.#saldo);
+            console.log("Seu novo saldo é = " + this._saldo);
+            return valorDeposito;
         }
     }
 }
@@ -39,7 +42,7 @@ const cliente2 = new Cliente();
 //Atribuindo as propriedades
 cliente2.nome ="Alice";
 cliente2.cpf = 11122233309;
-cliente2.rg = 999992999;
+cliente2.rg = 999992998;
 
 //Atribuindo valor a conta corrente/
 const contaCorrenteRicardo = new ContaCorrente();
@@ -51,9 +54,9 @@ let valorSacado = 200;
 let valorDepositado = 400;
 
 
-contaCorrenteRicardo.sacar(valorSacado);
-contaCorrenteRicardo.depositar(valorDepositado);
 
+contaCorrenteRicardo.depositar(valorDepositado);
+contaCorrenteRicardo.sacar(valorSacado);
 
 console.log(contaCorrenteRicardo);
 
